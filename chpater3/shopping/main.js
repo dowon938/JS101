f0baeaac4978e2f713f97ba65ca9dd3b05a0c9a7
dowon938 <68101878+dowon938@users.noreply.github.com>
2020-12-04 00:20:21 +0900

    shopping : review with ellie

--- chpater3/shopping/main.js
+++ chpater3/shopping/main.js
@@ -1,15 +1,15 @@
 'use strict';
 
-const createBtn = document.querySelector('.input-create');
-const inputText = document.querySelector('#input-text');
-const displayItem = document.querySelector('.display-item');
+const footerBtn = document.querySelector('.footer-btn');
+const footerInput = document.querySelector('#footer-input');
+const items = document.querySelector('.items');
 
 
 
 let i = 0;
 //Create Items
-function create(text) {
-    const item = document.createElement('div');
+function createItem(text) {
+    const item = document.createElement('li');
     const itemCheck = document.createElement('input');
     const label = document.createElement('label');
     const itemDelete = document.createElement('div');
@@ -17,13 +17,13 @@ function create(text) {
     const circle = document.createElement('label');
     
     item.setAttribute('class', 'item'); 
-    displayItem.appendChild(item);
+    items.appendChild(item);
     itemCheck.setAttribute('type','checkbox');
     itemCheck.setAttribute('class','item-check');
     itemCheck.setAttribute('id', i);
     check.setAttribute('class', 'check');
-    check.setAttribute('for', i);
     circle.setAttribute('class', 'circle');
+    check.setAttribute('for', i);
     circle.setAttribute('for', i);
     check.innerHTML = '<i class="fas fa-check-circle"></i>'
     circle.innerHTML = '<i class="far fa-circle"></i>'
@@ -37,6 +37,12 @@ function create(text) {
     item.appendChild(circle);
     item.appendChild(label);
     item.appendChild(itemDelete);
+    //scroll into item
+    item.scrollIntoView();
+    //Ellie's
+    // itemDelete.addEventListener('click', ()=>{
+    //     items.removeChild(item);
+    // })
     i=i+1;
 };
 // Delete Items
@@ -48,20 +54,47 @@ function deleteItem() {
 };
 
 function createAndDel() {
-    const text = inputText.value;
+    const text = footerInput.value;
     if (text != '') {
-        create(text);
+        createItem(text);
     };
     deleteItem();
     // Delete inputText Box
-    inputText.value = '';
+    footerInput.value = '';
+    footerInput.focus();
 };
 
 // Click event!
-createBtn.addEventListener('click',()=>createAndDel());
+footerBtn.addEventListener('click',()=>createAndDel());
 // Enter Key event!
-inputText.addEventListener("keyup", function(event) {
+footerInput.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
         createAndDel();
     };
 });    
+
+// ellie's!!*********
+
+// function onAdd() {
+    // 1.사용자가 입력한 텍스트 받아오기
+    // const text = footerInput.value;
+
+    //*빈텍스트입력시 커서만 유지하고 리턴
+    //if (text === '') {
+        // footerInput.focus();
+        // return;
+    // }
+
+    // 2.아이템생성 (삭제버튼 포함)
+    // const item = createItem(text);
+    // 3.아이템 배치 (items안으로)
+    // items.appendChild(item);
+    // 4.추가된 아이템으로 스크롤
+    // item.scrollIntoView();
+    // 5.인풋초기화
+    // footerInput.value = '';
+    // footerInput.focus();
+// }
+
+//코드의 기능을 그대로 설명하는 주석은 가독성을 해칠수있다.
+//가능하면 주석은 그 코드를 왜 작성하게되었는지 의도를 설명하라
